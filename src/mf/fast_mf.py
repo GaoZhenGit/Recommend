f9eeb1c448f90f7a83931eb5295c298f3fe80296
GaoZhen <912815282@qq.com>
2017-05-10 22:27:43 +0800

    适配fast mf的系列操作

--- src/mf/fast_mf.py
+++ src/mf/fast_mf.py
@@ -1,7 +1,12 @@
 import os
 
+import mf_matrix
+import mf_score
+import mf_result
+
 
 def run(param_path = './mf/config.json'):
+
     command = 'java -jar '
     jar_path = './mf/fast_mf.jar '
     exec_commend = command + jar_path + ' ' + param_path
@@ -10,4 +15,11 @@ def run(param_path = './mf/config.json'):
 
 
 if __name__ == '__main__':
+    print 'start read'
+    mf_matrix.read(-1)
+    print 'start mf'
     run()
+    print 'start score'
+    mf_score.run()
+    print 'start result'
+    mf_result.run()
